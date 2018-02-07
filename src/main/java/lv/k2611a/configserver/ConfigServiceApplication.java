db857c1a5dfb5ffd4a4833372a289e47646efac2
Kirill Afanasjev <kirill.afanasjev@odnoklassniki.ru>
2018-02-07 20:10:09 +0200

    config server

---
+++ src/main/java/lv/k2611a/configserver/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package lv.k2611a.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
